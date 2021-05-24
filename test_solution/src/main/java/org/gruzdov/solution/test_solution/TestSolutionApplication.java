f925b60636caa20d85a2cbd114496054a336f403
Vladislav Gruzdov <78535650+VodzurGoNe@users.noreply.github.com>
2021-05-24 13:11:04 +0400

    Add files via upload

---
+++ test_solution/src/main/java/org/gruzdov/solution/test_solution/TestSolutionApplication.java
@@ -0,0 +1,13 @@
+package org.gruzdov.solution.test_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSolutionApplication.class, args);
+    }
+
+}
---
+++ test_solution/src/test/java/org/gruzdov/solution/test_solution/TestSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gruzdov.solution.test_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
