d8c10a05f21b2775fd265634dbd61538b77bec2d
Vladislav Gruzdov <Gruzdov.V.1991@Hotmail.com>
2021-05-30 23:05:58 +0400

    "Prototype 5.3"

--- test_solution/src/test/java/test_solution/service/CalculationPaymentServiceTest.java
+++
@@ -1,7 +0,0 @@
-package test_solution.service;
-
-import test_solution.entity.CreditOffer;
-
-public interface CalculationPaymentServiceTest {
-    void calculationPaymentSchedule(CreditOffer creditOffer);
-}
