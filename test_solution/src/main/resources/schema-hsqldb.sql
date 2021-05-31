e768ab6a7237f22e8d11ebc310a3c89dd4f14392
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-05-31 19:18:09 +0400

    "Prototype 6.2"

--- test_solution/src/main/resources/schema-hsqldb.sql
+++ test_solution/src/main/resources/schema-hsqldb.sql
@@ -1,84 +1,102 @@
-CREATE TABLE PUBLIC.PUBLIC.bank
+CREATE TABLE PUBLIC.PUBLIC.BANK
 (
-    bank_id               uuid not null,
+    BANK_ID               uuid not null,
 
-    bank_title varchar(255),
+    BANK_TITLE varchar(255),
 
-    PRIMARY KEY (bank_id)
+    PRIMARY KEY (BANK_ID)
 );
 
-CREATE TABLE PUBLIC.PUBLIC.client
+CREATE TABLE PUBLIC.PUBLIC.CLIENT
 (
-    client_id             uuid not null
-        constraint client_pkey
+    CLIENT_ID             uuid not null
+        constraint CLIENT_pkey
             primary key,
-    client_email          varchar(205),
-    client_fio            varchar(205),
-    client_passportnumber varchar(205),
-    client_phonenumber    varchar(205),
-    bank_id               uuid
+    CLIENT_EMAIL          varchar(205),
+    CLIENT_FIO            varchar(205),
+    CLIENT_PASSPORTNUMBER varchar(205),
+    CLIENT_PHONENUMBER    varchar(205),
+    BANK_ID               uuid
         constraint fkim95abd01ot21q2dn9mpxo7nc
-            references bank
+            references BANK
 );
 
-create table PUBLIC.PUBLIC.credit
+create table PUBLIC.PUBLIC.CREDIT
 (
-    credit_id      uuid not null
-        constraint credit_pkey
+    CREDIT_ID      uuid not null
+        constraint CREDIT_pkey
             primary key,
-    credit_limit   numeric(19, 2)
-        constraint credit_credit_limit_check,
+    CREDIT_LIMIT   numeric(19, 2),
 
-    credit_percent numeric(19, 2)
-        constraint credit_credit_percent_check,
 
-    credit_title   varchar(255),
-    bank_id        uuid
+    CREDIT_PERCENT numeric(19, 2),
+
+
+    CREDIT_TITLE   varchar(255),
+    BANK_ID        uuid
         constraint fk1x1iaf0dsp6s1tjhdihhedgdk
-            references bank
+            references BANK
 );
 
-create table PUBLIC.PUBLIC.creditoffer
+create table PUBLIC.PUBLIC.CREDITOFFER
 (
-    creditoffer_id               uuid not null
-        constraint creditoffer_pkey
+    CREDITOFFER_ID               uuid not null
+        constraint CREDITOFFER_pkey
             primary key,
-    creditoffer_amount           numeric(19, 2),
+    CREDITOFFER_AMOUNT           numeric(19, 2),
 
 
-    creditoffer_firstpay         numeric(19, 2),
+    CREDITOFFER_FIRSTPAY         numeric(19, 2),
 
 
-    creditoffer_named            varchar(255),
-    creditoffer_percentsum       numeric(19, 2),
-    creditoffer_period_in_months integer,
+    CREDITOFFER_NAMED            varchar(255),
+    CREDITOFFER_PERCENTSUM       numeric(19, 2),
+    CREDITOFFER_PERIOD_IN_MONTHS integer,
 
 
-    bank_id                      uuid
+    BANK_ID                      uuid
         constraint fkkij8lijqb84vmmkcnhssnpeji
-            references bank,
-    client_id                    uuid
+            references BANK,
+    CLIENT_ID                    uuid
         constraint fk18hoi8h4wktsstqg7c6bot11q
-            references client,
-    credit_id                    uuid
+            references CLIENT,
+    CREDIT_ID                    uuid
         constraint fk385fjsia5r7avrt8pk7t1hchk
-            references credit
+            references CREDIT
 );
 
-create table PUBLIC.PUBLIC.paymentschedule
+create table PUBLIC.PUBLIC.PAYMENTSCHEDULE
 (
-    paymentschedule_id                    uuid not null
-        constraint paymentschedule_pkey
+    PAYMENTSCHEDULE_ID                    uuid not null
+        constraint PAYMENTSCHEDULE_pkey
             primary key,
-    paymentschedule_amount_of_the_body    numeric(19, 2),
-    paymentschedule_amount_of_the_percent numeric(19, 2),
-    paymentschedule_payment_amount        numeric(19, 2),
-    paymentschedule_payment_date          date,
-    paymentschedule_remains               numeric(19, 2),
-    creditoffer_id                        uuid
+    PAYMENTSCHEDULE_AMOUNT_OF_THE_BODY    numeric(19, 2),
+    PAYMENTSCHEDULE_AMOUNT_OF_THE_PERCENT numeric(19, 2),
+    PAYMENTSCHEDULE_PAYMENT_AMOUNT        numeric(19, 2),
+    PAYMENTSCHEDULE_PAYMENT_DATE          date,
+    PAYMENTSCHEDULE_REMAINS               numeric(19, 2),
+    CREDITOFFER_ID                        uuid
         constraint fk67sgkjw07dmkvmy1f6invw33k
-            references creditoffer
+            references CREDITOFFER
 );
 
-INSERT INTO bank
-(bank_id, bank_title) values ('31a80fb6-1ca4-435c-8fb3-2215d145db41', 'OP');
+INSERT INTO BANK (BANK_ID, BANK_TITLE)
+
+values ('11a80fb6-1ca4-435c-8fb3-2215d145db41', 'PRIME'),
+       ('21a80fb6-1ca4-435c-8fb3-2215d145db41', 'Second'),
+       ('31a80fb6-1ca4-435c-8fb3-2215d145db41', 'JBC'),
+       ('41a80fb6-1ca4-435c-8fb3-2215d145db41', ';)');
+
+INSERT INTO CREDIT (CREDIT_ID, CREDIT_TITLE, CREDIT_LIMIT, CREDIT_PERCENT, BANK_ID)
+
+values ('51a80fb6-1ca4-435c-8fb3-2215d145db41', 'Mortgage', 15000000, 8,'11a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('61a80fb6-1ca4-435c-8fb3-2215d145db41', 'Сonsumer', 700000, 30, '21a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('71a80fb6-1ca4-435c-8fb3-2215d145db41', 'Auto credit', 10000000, 15, '31a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('81a80fb6-1ca4-435c-8fb3-2215d145db41', '"installment plan"', 150000, 6, '41a80fb6-1ca4-435c-8fb3-2215d145db41');
+
+INSERT INTO CLIENT (CLIENT_ID, CLIENT_FIO, CLIENT_PHONENUMBER, CLIENT_EMAIL, CLIENT_PASSPORTNUMBER, BANK_ID)
+
+values ('91a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Vladislav Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '11a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('62a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Vladimir Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '21a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('73a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Valeriy Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '31a80fb6-1ca4-435c-8fb3-2215d145db41'),
+       ('84a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov =P Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '41a80fb6-1ca4-435c-8fb3-2215d145db41');
