4e2426f2ced749875028cae5e73ab380f13b88e3
Vladislav Gruzdov <Gruzdov.V.1991@Hotmail.com>
2021-06-01 20:06:14 +0400

    "Prototype 6.6"

--- test_solution/src/main/resources/schema-hsqldb.sql
+++ test_solution/src/main/resources/schema-hsqldb.sql
@@ -62,31 +62,3 @@ create table PUBLIC.PUBLIC.PAYMENT_SCHEDULE (
     constraint fk67sgkjw07dmkvmy1f6invw33k
     references CREDIT_OFFER
 );
-
-INSERT INTO BANK (BANK_ID, BANK_TITLE)
-
-values ('11a80fb6-1ca4-435c-8fb3-2215d145db41', 'PRIME'),
-       ('21a80fb6-1ca4-435c-8fb3-2215d145db41', 'Second'),
-       ('31a80fb6-1ca4-435c-8fb3-2215d145db41', 'JBC'),
-       ('41a80fb6-1ca4-435c-8fb3-2215d145db41', ';)');
-
-INSERT INTO CREDIT (CREDIT_ID, CREDIT_TITLE, CREDIT_LIMIT, CREDIT_PERCENT, BANK_ID)
-
-values ('51a80fb6-1ca4-435c-8fb3-2215d145db41', 'Mortgage', 15000000, 8,'11a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('61a80fb6-1ca4-435c-8fb3-2215d145db41', 'Сonsumer', 700000, 30, '21a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('71a80fb6-1ca4-435c-8fb3-2215d145db41', 'Auto credit', 10000000, 15, '31a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('81a80fb6-1ca4-435c-8fb3-2215d145db41', '"installment plan"', 150000, 6, '41a80fb6-1ca4-435c-8fb3-2215d145db41');
-
-INSERT INTO CLIENT (CLIENT_ID, CLIENT_FIO, CLIENT_PHONE_NUMBER, CLIENT_EMAIL, CLIENT_PASSPORT_NUMBER, BANK_ID)
-
-values ('91a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Vladislav Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '11a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('62a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Vladimir Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '21a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('73a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Valeriy Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '31a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('84a80fb6-1ca4-435c-8fb3-2215d145db41', 'Gruzdov Miroslav Grigorevich', '907-964-39-63', 'tomcat@random.com', '31-11-502830', '41a80fb6-1ca4-435c-8fb3-2215d145db41');
-
-INSERT INTO CREDIT_OFFER (CREDIT_OFFER_ID, CREDIT_OFFER_NAMED, CREDIT_OFFER_AMOUNT, CREDIT_OFFER_PERIOD_IN_MONTHS, CREDIT_OFFER_FIRST_PAY, CREDIT_OFFER_PERCENT_SUM, BANK_ID, CREDIT_ID, CLIENT_ID)
-
-values ('91a80fb6-1ca4-435c-8fb3-2216d145db41', 'Spring', 100000, 2, 10, 8, '11a80fb6-1ca4-435c-8fb3-2215d145db41', '51a80fb6-1ca4-435c-8fb3-2215d145db41', '91a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('62a80fb6-1ca4-435c-8fb3-2217d145db41', 'Joy', 150000, 2, 10, 30, '21a80fb6-1ca4-435c-8fb3-2215d145db41', '61a80fb6-1ca4-435c-8fb3-2215d145db41', '62a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('73a80fb6-1ca4-435c-8fb3-2218d145db41', 'Pull', 150800, 2, 10, 15, '31a80fb6-1ca4-435c-8fb3-2215d145db41', '71a80fb6-1ca4-435c-8fb3-2215d145db41', '73a80fb6-1ca4-435c-8fb3-2215d145db41'),
-       ('84a80fb6-1ca4-435c-8fb3-2219d145db41', 'Optimal', 180000, 2, 10, 6, '41a80fb6-1ca4-435c-8fb3-2215d145db41', '81a80fb6-1ca4-435c-8fb3-2215d145db41', '84a80fb6-1ca4-435c-8fb3-2215d145db41');
