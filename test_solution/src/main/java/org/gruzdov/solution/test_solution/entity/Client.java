778e580533214fb6030e18c27eab49c65a78d982
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-06-11 18:48:46 +0400

    Refactor Client Class: improve the readability of @Pattern regexp and message fields fio, phoneNumber, passportNumber

--- test_solution/src/main/java/org/gruzdov/solution/test_solution/entity/Client.java
+++ test_solution/src/main/java/org/gruzdov/solution/test_solution/entity/Client.java
@@ -21,23 +21,26 @@ public class Client {
     private UUID id;
 
     @NotBlank(message = "FIO is required field")
-    @Pattern(regexp = "^[a-zA-ZА-Яа-я '-]{2,}([-][a-zA-ZА-ЯЁ][а-яё]{2,})?\\s[a-zA-ZА-ЯЁ][a-zA-ZАа-яё]{2,}\\s[a-zA-ZА-ЯЁ][a-zA-ZАа-яё]{2,}$",
-            message = "Please use pattern \"Last Name First Name Middle Name\"")
+    @Pattern(regexp = "^[a-zA-ZА-Яа-я '-]{2,}([-][a-zA-ZА-ЯЁ][а-яё]{2,})?\\s[a-zA-ZА-ЯЁ][a-zA-ZАа-яё]{2,}" +
+            "\\s[a-zA-ZА-ЯЁ][a-zA-ZАа-яё]{2,}$", message = "Please use pattern \"Last Name First Name Middle Name\" " +
+            "for example \"Gruzdov Vladislav Grigoryevich\"")
     @Column(name = "CLIENT_FIO")
     private String fio;
 
     @NotBlank(message = "Phone Number is required field")
-    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{2}-\\d{2}", message = "Please use pattern XXX-XXX-XX-XX")
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{2}-\\d{2}",
+            message = "Please use pattern XXX-XXX-XX-XX for example \"987-964-39-60\"")
     @Column(name = "CLIENT_PHONE_NUMBER")
     private String phoneNumber;
 
-    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
-            message = "Please use pattern \"login@siteAddress.domainName\" for example \"bestuser@gmail.com\"")
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*" +
+            "(\\.[A-Za-z]{2,})$", message = "Please use pattern \"login@siteAddress.domainName\" " +
+            "for example \"bestuser@gmail.com\"")
     @Column(name = "CLIENT_EMAIL")
     private String email;
 
     @NotBlank(message = "Passport Number is required field")
-    @Pattern(regexp = "\\d{2}-\\d{2}-\\d{6}", message = "Please use pattern XX-XX-XXXXXX")
+    @Pattern(regexp = "\\d{2}-\\d{2}-\\d{6}", message = "Please use pattern XX-XX-XXXXXX for example \"36-11-502830\"")
     @Column(name = "CLIENT_PASSPORT_NUMBER")
     private String passportNumber;
 
